71dc12b345e48ebb2b52a5d8eddfcbfafed34ad3
Melih Sünbül <67157758+melihsunbul@users.noreply.github.com>
2025-06-24 14:19:45 +0200

    change moduleResoulution type and resolved package version dependency problems

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="streamlit-keycloak-lex",
-    version="1.0.3",
+    version="1.0.4",
     author="Melih Sünbül",
     author_email="m.sunbul@lund-it.com",
     description="Keycloak authentication for Streamlit",
